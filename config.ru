0e300dc49e85459db5bad031b2cd20fa463b5b91
Haroon <haroon20@hotmail.co.uk>
2022-02-23 16:27:42 +0000

    CSS styles

--- config.ru
+++ config.ru
@@ -2,6 +2,4 @@
 
 require_relative './app'
 
-
-
 run Mahah
--- database_setup.rb
+++ database_setup.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 require 'sequel'
 require 'dotenv/load'
 
-
 if ENV['RACK_ENV'] == 'test'
   test_connection = "postgres://#{ENV['TEST_DB_USER']}:#{ENV['TEST_DB_PASSWORD']}@#{ENV['TEST_HOST']}:#{ENV['TEST_PORT']}/makersbnb_test"
-  DB = Sequel.connect(test_connection) 
-else #if in Development
+  DB = Sequel.connect(test_connection)
+else # if in Development
   dev_connection = "postgres://#{ENV['DEV_DB_USER']}:#{ENV['DEV_DB_PASSWORD']}@#{ENV['DEV_HOST']}:#{ENV['DEV_PORT']}/makersbnb"
   DB = Sequel.connect(dev_connection)
 end
