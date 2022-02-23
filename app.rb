f54eb40927aff6dcb59509a4a6a7216a12bba693
Dashiell Boyles <dashiellboyles@Dashiells-MacBook-Air.local>
2022-02-23 20:00:11 +0000

    Confirmation that you have booked a space

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 require './database_setup'
 require './lib/space'
+require './lib/booking'
 
 # Maker House a Home
 class Mahah < Sinatra::Base
@@ -20,29 +21,29 @@ class Mahah < Sinatra::Base
   end
 
   post '/spaces' do
-    # p params
     Space.create(name: params[:name], description: params[:description], price: params[:price], owner_customer_id: params[:owner_customer_id])
-    
     redirect to "/spaces"
   end
 
   get '/spaces' do
     @spaces = Space.all
-    
     erb :"spaces/index"
-    
   end
 
   get '/bookings/new' do
-
+    @spaces = Space.all
     erb :"bookings/new"
-
   end
 
-  get '/bookings' do
+  # get '/bookings' do
+  #   erb :"bookings/index"
+  # end
+
+  post '/bookings' do
+    @owner = params[:owner]
+    @property = params[:property] 
     erb :"bookings/index"
   end
-
   
   run! if app_file == $PROGRAM_NAME
 end
